798bf96675e9ad385442c2c28c4fc1155aa6a76c
agc15000 <no1basketballplayer15@gmail.com>
2025-03-22 22:50:57 +0900

    add rails_admin,carrierwave

--- app/models/monster.rb
+++ app/models/monster.rb
@@ -1,4 +1,7 @@
 class Monster < ApplicationRecord
   has_many :user_monsters
   has_many :monster, through: :user_monsters
+  #has_one_attached :image　carrierwaveを使用したためコメントアウト
+  #1つのモンスター画像と紐づく
+  mount_uploader :image, MonsterImageUploader #carrierwaveを使用
 end
--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,9 @@ module MottainaiMonsters2
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.1
 
+    config.i18n.default_locale = :ja
+    #日本語化
+
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
